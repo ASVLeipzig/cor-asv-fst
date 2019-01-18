d25d8840c4d478a8c8bbd465beef26d6576fd598
Maciej Sumalvico <sumalvico@informatik.uni-leipzig.de>
2019-01-18 14:44:56 +0100

    Merged error transducer creating scripts
    Merged `error_transducer_complete.py` into `error_transducer.py`, so that one
    module is responsible for training an error model.

--- hfst/error_transducer.py
+++ hfst/error_transducer.py
@@ -344,6 +344,7 @@ def no_punctuation_edits(confusion):
             return False
     return True
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description='OCR post-correction ocrd-cor-asv-fst error model creator')
@@ -360,9 +361,14 @@ def parse_arguments():
         '-C', '--max-context', metavar='NUM', type=int, default=3,
         help='maximum size of context count edits at')
     parser.add_argument(
+        '-E', '--max-errors', metavar='NUM', type=int, default=3,
+        help='maximum number of errors the resulting FST can correct '
+             '(applicable within one window, i.e. a certain number of words)')
+    parser.add_argument(
         '-P', '--preserve-punctuation', action='store_true', default=False,
         help='ignore edits to/from non-alphanumeric or non-space characters')
     return parser.parse_args()
+
 
 def main():
     """
@@ -433,6 +439,53 @@ def main():
 
         # write transducer to file
         error_transducer.write_to_file('error_transducer_' + str(n) + '.hfst')
+
+    ##########################
+    # error_transducer_complete.main() starts here
+
+    error_transducers = {}
+    for n in range(1,args.max_errors+1):
+        error_transducers[n] = hfst.HfstTransducer.read_from_file('error_transducer_' + str(n) + '.hfst')
+
+    contexts = []
+    for n in range(1,args.max_context+1):
+        for m in range(1,n+1):
+            contexts.append(list(range(m,n+1)))
+    
+    # FIXME: make proper back-off transducer
+    
+    acceptor = hfst.regex('?')
+    acceptor.repeat_star()
+    acceptor.minimize()
+
+    for context in contexts:
+
+        print('Context: ', context)
+
+        error_transducer = hfst.HfstTransducer()
+        
+        if n in context:
+            error_transducer.disjunct(error_transducers[n])
+        
+        one_error = acceptor.copy()
+        one_error.concatenate(error_transducer)
+        #one_error.optionalize()
+        
+        # TODO: find out why both minimize and minimize+remove_epsilons makes FSTs extremely huge and slow at runtime
+        # TODO: find out why incremental concat+disjoin with previous ET (starting with all-acceptor) is larger and slower
+        #error_transducer = acceptor.copy()
+        for num_errors in range(1,args.max_errors+1):
+            print('Number of errors:', num_errors)
+            result_transducer = one_error.copy()
+            #result_transducer.repeat_n(num_errors) # instead of reapeat_n_minus: together with optionalize above
+            result_transducer.repeat_n_minus(num_errors)
+            #result_transducer.concatenate(error_transducer) # N …
+            #result_transducer.disjunct(error_transducer) # … ∪ N-1
+            
+            error_transducer =  result_transducer
+            error_transducer.concatenate(acceptor)
+            
+            error_transducer.write_to_file('max_error_' + str(num_errors) + '_context_'+ ''.join(map(str,context)) + '.hfst')
 
 if __name__ == '__main__':
     main()
